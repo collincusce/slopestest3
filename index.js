9104e064008fe627d701acc1ea0cacbf83947d38
Collin K. Cusce <collin@avalabs.org>
2020-01-31 15:17:31 -0500

    temporary tests

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const Buffer = require('buffer/').Buffer;
 
 let bintools = slopes.BinTools.getInstance();
 
-let ava = new slopes.Slopes("localhost", 9650, "http", 12345);
+let ava = new slopes.Slopes("localhost", 9650, "http", 12345, "2PfbSxTqpTGFF2xCX2YgrW6ncrksfmEhcNXGv9rE9CgTRqT4hM");
 let avm = ava.AVM(); //returns a reference to the AVM API used by Slopes
 
 let managekeys = async () => {
     let myKeychain = avm.keyChain();
     let newAddress1 = myKeychain.makeKey();
-    let mypk = Buffer.from("330530eda3225d280d42efc5f02d31d122da3da3093c739ddd7d16612c7dfd53", "hex");
-    let newAddress2 = myKeychain.importKey(mypk);
+    let mypk = bintools.avaDeserialize("24jUJ9vZexUM6expyMcT48LBx27k1m7xpraoV62oSQAHdziao5");
+    let newAddress2 = myKeychain.importKey(mypk).toString();
     let addresses = myKeychain.getAddresses(); //returns an array of all addresses managed
     let exists = myKeychain.hasKey(newAddress1); //returns true if the address is managed
     let keypair = myKeychain.getKey(newAddress1); //returns the keypair class
@@ -26,8 +26,8 @@ let managekeys = async () => {
     //creating a new random keypair
     keypair = new slopes.AVMKeyPair(); 
     keypair.generateKey();
-    let mypk2 = Buffer.from("021ee5e48e70e336d82d894c8f80c8109fd75651720cf662661236d22ab7b7b6", "hex");
-    let successful = keypair.importKey(mypk2); //returns boolean if private key imported successfully
+    //let mypk2 = Buffer.from("021ee5e48e70e336d82d894c8f80c8109fd75651720cf662661236d22ab7b7b6", "hex");
+    //let successful = keypair.importKey(mypk2); //returns boolean if private key imported successfully
 
     let message = "Wubalubadubdub";
     let signature = keypair.sign(message); //returns a Buffer with the signature
@@ -70,10 +70,36 @@ let sendingassets = async (utxos, assetid) => {
     let friendsAddress = "B6D4v1VtPYLbiUvYXtW4Px8oE9imC2vGW"; //AVA serialized address format
     let unsignedTx = avm.makeUnsignedTx(utxos, sendAmount, [friendsAddress], myAddresses, myAddresses, assetid); 
     let signedTx = avm.signTx(unsignedTx);
+    console.log("SIGNED", signedTx.toString("hex"));
     let txid = await avm.issueTx(signedTx);
 
     return txid;
 }
+
+let assetID = bintools.avaDeserialize("2KBJoSQV3aQ4SHrZh3HRXacFZhEvdmCRsQV1JuLzuqeWytiKWn");
+
+managekeys().then(() => {
+    let boyyy = avm.keyChain().getAddresses();
+    console.log("boyyy", boyyy);
+    avm.getUTXOs(boyyy).then((result) => {
+        let cb = async (txid, assetid) => {  
+            let myAddresses = avm.keyChain().getAddresses(); //returns an array of addresses the keychain manages
+            let sendAmount = new BN(100); //amounts are in BN format
+            // returns one of: "Accepted", "Processing", "Unknown", and "Rejected"
+            let status = await avm.getTxStatus(txid);
+            console.log(status);
+            let updatedUTXOs = await avm.getUTXOs();
+            console.log("updatedUTXOs", updatedUTXOs);
+            let newBalance = updatedUTXOs.getBalance(myAddresses, assetid);
+            console.log("New Balance ")
+        };
+        sendingassets(result, assetID).then((txid) => {
+            setTimeout(() => cb(txid, assetID), 5000);
+        })
+    });
+});
+
+/*
 let assetIDList = [];
 let utxosGlobal;
 managekeys().then(() => {
@@ -113,4 +139,4 @@ managekeys().then(() => {
         });
     }, 5000);
 });
-
+*/
